0febb71d3e2ce7591d158c2d68542174e72f56fd
irishkooky <yoshihiro.kurosaki.15@gmail.com>
2023-11-03 15:47:55 +0900

    🚧

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image'
+import { Suspense } from 'react'
 import NotesList from './components/notes-list'
+import TimerCounter from './components/timer-counter'
+import Spinner from './components/spinner'
 
 export default function Page() {
   return (
     <main>
       <div className="m-10 text-center">
         <p>Hello World🚀</p>
-        {/* @ts-ignore */}
-        <NotesList />
+        <Suspense fallback={<Spinner color="border-green-500" />}>
+          {/* @ts-ignore */}
+          <NotesList />
+        </Suspense>
+        <TimerCounter />
       </div>
     </main>
   )
