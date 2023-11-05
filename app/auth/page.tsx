d0ebc140a93573e052d33debf1ee3a3aeb14b14b
irishkooky <yoshihiro.kurosaki.15@gmail.com>
2023-11-05 13:32:01 +0900

    ✨ Add auth

---
+++ app/auth/page.tsx
@@ -0,0 +1,11 @@
+import Auth from '../components/auth'
+
+export default async function AuthPage() {
+  return (
+    <main
+      className={`h-[calc(100vh-56px flex flex-col items-center justify-center`}
+    >
+      <Auth />
+    </main>
+  )
+}
